3fa1f61e8dd253e7300d9d08bdc6e33664838b71
IshaCodes04 <ishu010.com@gmail.com>
2025-12-27 15:01:10 +0530

    db.js created

--- Razorpay-1/Backend/src/db/db.js
+++ Razorpay-1/Backend/src/db/db.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 
+
+async function ConnectToDB() {
+  try{
+      await mongoose.connect()
+  }catch(error){
+      console.error("Error while connecting to the database" , error);
+      process.exit(1); // use to shutdown server automatically
+  }
+}
+
+
+
+module.exports = ConnectToDB;
