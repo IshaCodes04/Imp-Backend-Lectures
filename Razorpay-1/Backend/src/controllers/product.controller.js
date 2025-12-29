8d6c4df57be26821c0af83a8a2561fe170f565c1
IshaCodes04 <ishu010.com@gmail.com>
2025-12-29 16:05:04 +0530

    getProduct

--- Razorpay-1/Backend/src/controllers/product.controller.js
+++ Razorpay-1/Backend/src/controllers/product.controller.js
@@ -2,29 +2,43 @@ const productModel = require("../models/product.model");
 
 
 async function createProduct(req, res) {
-   
-    const { image, title, description, price: {amount , currency}, category } = req.body // yh data body m ayega
 
-    try{
-       const product = await productModel.create({ // product created
-          image, title, description, price: {amount , currency}, category
-       })
+   const { image, title, description, price: { amount, currency }, category } = req.body // yh data body m ayega
 
-       return res.status(201).json({
-          message: "Product created successfully!",
-          product
-       })
+   try {
+      const product = await productModel.create({ // product created
+         image, title, description, price: { amount, currency }, category
+      })
 
-    }catch (error) {
-       return res.status(500).json({
-          message: "Something went wrong",
-          error: error.message
-       })
-    }
+      return res.status(201).json({
+         message: "Product created successfully!",
+         product
+      })
+
+   } catch (error) {
+      return res.status(500).json({
+         message: "Something went wrong",
+         error: error.message
+      })
+   }
 }
 
-
+async function getProduct(req, res) {
+   try {
+      const product = await productModel.findOne()
+      return res.status(200).json({
+         message: "Product fetched successfully",
+         product
+      })
+   } catch (error) {
+      return res.status(500).json({
+         message: "Something went wrong",
+         error: error.message
+      })
+   }
+}
 
 module.exports = {
-   createProduct
+   createProduct,
+   getProduct
 }
