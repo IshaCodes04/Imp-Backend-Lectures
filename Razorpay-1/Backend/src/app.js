134c868dd364d2c0e01a7279eee97ba0c0f6ea94
IshaCodes04 <ishu010.com@gmail.com>
2025-12-29 13:58:31 +0530

    add preffix of product api

--- Razorpay-1/Backend/src/app.js
+++ Razorpay-1/Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const productRoutes =  require("../src/routes/product.routes");
 
 // server created
 const app = express();
@@ -6,6 +7,9 @@ const app = express();
 // middleware for take data in req.body
 app.use(express.json());
 
+
+// preffix for our product api
+app.use('/api/products' , productRoutes);
 
 
 
