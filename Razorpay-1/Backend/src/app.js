06d0fcc479adf8e86f1d5c5591b36135dc33e1da
IshaCodes04 <ishu010.com@gmail.com>
2025-12-29 16:37:27 +0530

    cors added

--- Razorpay-1/Backend/src/app.js
+++ Razorpay-1/Backend/src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const productRoutes =  require("./routes/product.routes");
+const cors = require("cors");
 
 // server created
 const app = express();
 
+
+// use cors 
+app.use(cors());
 // middleware for take data in req.body
 app.use(express.json());
 
 
+
 // preffix for our product api
 app.use('/api/products', productRoutes);
+
 
 
 
